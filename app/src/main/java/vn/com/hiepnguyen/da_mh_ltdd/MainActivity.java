ea43763d8fad89d41bc4163f07aae4721a9767f1
nguyenhiephttt <51333958+nguyenhiephttt@users.noreply.github.com>
2019-06-06 15:24:30 +0700

    Initial commit

---
+++ app/src/main/java/vn/com/hiepnguyen/da_mh_ltdd/MainActivity.java
@@ -0,0 +1,13 @@
+package vn.com.hiepnguyen.da_mh_ltdd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
